6f9bbd81ea5f0d8e7e861aa14e6477da41410a24
root <root@localhost.localdomain>
2020-06-02 15:39:10 -0400

    se agrega mystyles.css al head

--- app/Views/templates/head.php
+++ app/Views/templates/head.php
@@ -7,10 +7,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<!-- Foundation -->
+	<link rel="stylesheet" href="css/foundation.css" />
 
 	<!-- STYLES -->
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- My Custom Styles -->
+    <link rel="stylesheet" href="assets/mystyles.css">
 
     <style type="text/css">  
       .carousel-inner img {  
